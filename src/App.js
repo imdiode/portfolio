a9b11da174289f287348a7cb7479245302dc5063
imdiode <nandanparagshukla@gmail.com>
2022-05-09 08:56:52 +0530

    Header updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { IconButton } from "@chakra-ui/button";
+import { useColorMode } from "@chakra-ui/color-mode";
+import { Flex, VStack, Heading, Spacer } from "@chakra-ui/layout";
+import {
+  FaSun,
+  FaMoon,
+  FaInstagram,
+  FaGithub,
+  FaLinkedin,
+  FaTwitter
+} from "react-icons/fa";
+import Header from "./components/Header";
+import Profile from "./components/Profile";
+import Social from "./components/Social";
 
 function App() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isDark = colorMode === "dark";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VStack p={5}>
+      <Flex w="100%">
+        <Heading ml="8" size="md" fontWeight="semibold" color="cyan.400">
+          <Flex>
+            Diode&nbsp;
+            <img alt="diodeIcon" src="/diode.svg" />
+          </Flex>
+        </Heading>
+
+        <Spacer></Spacer>
+        <IconButton
+          icon={<FaLinkedin />}
+          isRound="true"
+          onClick={() => window.open("https://www.linkedin.com/in/diode16/")}
+        ></IconButton>
+        <IconButton
+          ml={2}
+          icon={<FaInstagram />}
+          isRound="true"
+          onClick={() => window.open("https://www.instagram.com/diode.h")}
+        ></IconButton>
+        <IconButton
+          ml={2}
+          icon={<FaTwitter />}
+          isRound="true"
+          onClick={() => window.open("https://www.twitter.com/diode_tech")}
+        ></IconButton>
+        <IconButton
+          ml={2}
+          icon={<FaGithub />}
+          isRound="true"
+          onClick={() => window.open("https://www.github.com/imdiode")}
+        ></IconButton>
+        <IconButton
+          ml={8}
+          icon={isDark ? <FaSun /> : <FaMoon />}
+          isRound="true"
+          onClick={toggleColorMode}
+        ></IconButton>
+      </Flex>
+      <Header></Header>
+      <Social></Social>
+      <Profile></Profile>
+    </VStack>
   );
 }
 
